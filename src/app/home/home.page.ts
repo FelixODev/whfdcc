f771734fc3a4c21042200f5604c33e9dab1653c5
Felix O <felix.o.stack@gmail.com>
2022-05-25 23:40:23 +0000

    ux stuff nice

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,40 @@
 import { Component } from '@angular/core';
 
+export const aisles = [
+  'Frozen','Cold Box','Soup Bar','Cookie',
+  'Detergent','Drinks','Baking','International',
+  'Chips','Bulk','Register'
+];
+
+interface Ca {
+  aisle: typeof aisles[number];
+  start: Date;
+  stop: Date;
+  count: number|0;
+};
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  cList:any[]= [
-    {aisle: "Frozen", start:"", stop:"", count: 0}
+  as:string[] = aisles;
+  cList:Ca[]= [
+    <Ca>{}
   ];
 
   constructor() {}
+
+  add() {
+    this.cList.push(<Ca>{});
+  }
+
+  setTime(prop) {
+    console.log(prop);
+    if(prop!=null||undefined)
+      return prop
+    return (new Date().toLocaleTimeString('en-GB')).slice(0, -3);
+  }
 
 }
