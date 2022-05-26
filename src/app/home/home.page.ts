bc4e9f9515e4ad7e75483a1e758f6c537280733c
Felix O <felix.o.stack@gmail.com>
2022-05-26 19:21:53 +0000

    calculations

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,6 +13,11 @@ interface Ca {
   count: number|0;
 };
 
+const tm = (t) => {
+  const m = t.split(":");
+  return m[0]*60+(+m[1])
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -35,6 +40,20 @@ export class HomePage {
     if(prop!=null||undefined)
       return prop
     return (new Date().toLocaleTimeString('en-GB')).slice(0, -3);
+  }
+
+  et(c) {
+    if(!c.start||!c.stop)
+      return 0
+      const t = tm(c.stop)-tm(c.start)
+    return (t>0)?t:+t+1440
+  }
+
+  cph(c) {
+    if(!c.start||!c.stop||!c.count)
+      return 0
+    return c.count
+    // /(+this.et(c)/60)
   }
 
   refresh(){
