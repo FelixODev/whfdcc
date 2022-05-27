3622c37c58e7eda92cba11c4003e7f222a93dfbb
Felix O <felix.o.stack@gmail.com>
2022-05-27 21:35:13 +0000

    local storage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 export const aisles = [
   'Frozen','Cold Box','Soup Bar','Cookie',
@@ -13,10 +14,21 @@ interface Ca {
   count: number|0;
 };
 
+interface Cw {
+  date?: string;
+  aisles?: Ca[];
+}
+
 const tm = (t) => {
   const m = t.split(":");
   return m[0]*60+(+m[1])
 }
+
+const dt = () => {
+  return ((new Date()).toISOString().split('T'))[0];
+}
+
+const wl = window.localStorage;
 
 @Component({
   selector: 'app-home',
@@ -25,11 +37,43 @@ const tm = (t) => {
 })
 export class HomePage {
   as:string[] = aisles;
+  dates: any = {[dt()]:true};
+  cWork:Cw = <Cw>{date:dt(),aisles:<Ca[]>[]};
   cList:Ca[]= [
     <Ca>{}
   ];
 
-  constructor() {}
+  constructor(
+    public alert: AlertController
+  ) {
+    this.init().then().catch()
+  }
+
+  async init() {
+    this.dates = JSON.parse(wl.getItem('dates'));
+    const cw = wl.getItem(dt());
+    if(cw){
+      this.cWork = JSON.parse(cw);
+      this.cList = this.cWork.aisles;
+    }
+  }
+
+  async refresh(){
+    await this.deInit();
+    window.location.reload();
+  }
+
+  async deInit() {
+    const u = {[this.cWork.date]: true};
+    this.dates = (this.dates)?{...this.dates,...u}:u;
+    wl.setItem('dates', JSON.stringify(this.dates));
+    this.cWork.aisles = this.cList;
+    wl.setItem(this.cWork.date,
+      JSON.stringify(
+        this.cWork
+      )
+    );
+  }
 
   add() {
     this.cList.push(<Ca>{});
@@ -73,8 +117,16 @@ export class HomePage {
     return Math.round(ct/((t<0.01)?1:t))
   }
 
-  refresh(){
-    window.location.reload();
+  async show(m) {
+    console.log(m);
+    const a = await this.alert.create({
+      message: m,
+      buttons: [{
+        text: 'Ok',
+        role: 'dismiss'
+      }]
+    });
+    await a.present();
   }
 
 }
